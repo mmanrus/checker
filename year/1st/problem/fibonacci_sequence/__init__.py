14e4b9bff5235af154fa8b4340107476ac287312
mmanrus <mmanrusiana@gmail.com>
2025-02-24 01:32:33 +0800

    removing stderr()

--- year/1st/problem/fibonacci_sequence/__init__.py
+++ year/1st/problem/fibonacci_sequence/__init__.py
@@ -6,21 +6,15 @@ def exists():
      check50.exists('fibonacci.py')
      
 @check50.check(exists)
-def print_fib():
-     actual = check50.run("python3 fibonacci.py 5").stdout()
-     if "5" not in actual and "3" in actual:
-          help = "Be sure to start the sequence at 1"
-          raise check50.Missing("5", actual, help=help)
-@check50.check(exists)
 def check_valueerror():
-     actual = check50.run('python3 fibonnacci.py aa').stderr()# prints to sys.exit or ValueError
-     if 'ERROR: Please enter an integer' not in actual:
-          help = "ValueError: Be sure to convert argv[1] to int"
-          raise check50.Mismatch('ERROR: Please enter an integer', actual, help=help)
+    actual = check50.run("python3 fibonacci.py aa").stdout()  # No .stderr(), use .stdout()
+    if "ERROR: Please enter an integer" not in actual:
+        help = "ValueError: Be sure to convert argv[1] to int"
+        raise check50.Mismatch("ERROR: Please enter an integer", actual, help=help)
+
 @check50.check(exists)
 def check_outofbounds():
-     actual = check50.run('python3 fibonnacci.py 1 2').stderr() # prints to sys.exit or ValueError
-     if 'USAGE: python fibonacci.py (n)' not in actual:
-          help = "Ensure the script only accepts a single argument: 'python fibonacci.py <n>'"
-          raise check50.Mismatch('USAGE: python fibonacci.py (n)', actual, help=help)
-     
+    actual = check50.run("python3 fibonacci.py 1 2").stdout()  # No .stderr(), use .stdout()
+    if "USAGE: python fibonacci.py (n)" not in actual:
+        help = "Ensure the script only accepts a single argument: 'python fibonacci.py <n>'"
+        raise check50.Mismatch("USAGE: python fibonacci.py (n)", actual, help=help)
