0a87183f7f53307ae4e8a789c415259e525aad33
mmanrus <mmanrusiana@gmail.com>
2025-03-19 14:23:00 +0800

    Adding addition_subtraction check

--- c/easy/addtion_subtraction/__init__.py
+++ c/easy/addtion_subtraction/__init__.py
@@ -12,20 +12,18 @@ def compiles():
 def checkFunctions():
      result = check50.run("./addition_subtraction").stdin("12 34").stdout()
      
-     expected_sum = "Sum: 46"
-     expected_difference = "Difference: -22"
+     expected_output = "Sum: 46\nDifference: -22"
+    
 
-     if result.match(expected_sum) and result.match(expected_difference) not in result:
-        help = "Error Running "
+     if expected_output not in result:
+        help = "Error Running: Output should be:\nSum: 46\nDifference: -22"
         raise check50.Mismatch("ERROR: Please enter an integer", result, help=help)
 
 @check50.check(compiles)
 def checkAgain():
      result = check50.run("./addition_subtraction").stdin("20 25").stdout()
-     expected_sum = "Sum: 45"
-     expected_difference = "Difference: -5"
-     result.match(expected_sum)
-     result.match(expected_difference)
-     if result.match(expected_sum) and result.match(expected_difference) not in result:
-        help = "Error Running "
+     expected_output = "Sum: 45\nDifference: -5"
+
+     if expected_output not in result:
+        help = "Error Running: Output should be:\nSum: 46\nDifference: -22"
         raise check50.Mismatch("ERROR: Please enter an integer", result, help=help)
